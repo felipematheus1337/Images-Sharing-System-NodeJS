d701e3b2be44093568e56b23caa2a289dc1306aa
felipematheus1337 <lipehbr@gmail.com>
2022-10-27 18:57:11 -0300

    user created test successfull

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
+let user = require("./models/User");
+
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
@@ -11,11 +13,26 @@ mongoose.connect("mongodb://localhost:27017/guiapics",{useNewUrlParser:true, use
     console.log(e);
 })
 
+let User = mongoose.model("User",user);
 
 
 app.get("/",(req,res) => {
     res.json({});
 })
+
+app.post("/user",async (req,res) => {
+    try {
+        let newUser  = new User({name:req.body.name,
+             email:req.body.email,password:req.body.password});
+        await newUser.save();
+        res.json({email:req.body.email})
+    } catch(e) {
+      res.sendStatus(500);
+    }
+    
+})
+
+
 
 
 module.exports = app;
